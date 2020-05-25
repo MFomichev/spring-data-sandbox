9d7803dab353a55041cecc37e5f47d5e76f74951
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-05-25 21:39:32 +0300

    Unpack project from spring initializr

---
+++ src/main/java/xyz/fomichev/sandbox/SpringDataSandboxApplication.java
@@ -0,0 +1,13 @@
+package xyz.fomichev.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/fomichev/sandbox/SpringDataSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.fomichev.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
