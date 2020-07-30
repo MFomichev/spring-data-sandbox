7e860699fa8eac1b49c05cc613e1ff26b7e54460
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-07-30 20:30:49 +0300

    fix failed test

--- src/test/java/xyz/fomichev/sandbox/service/ReadingSessionServiceTest.java
+++ src/test/java/xyz/fomichev/sandbox/service/ReadingSessionServiceTest.java
@@ -3,10 +3,18 @@ package xyz.fomichev.sandbox.service;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import liquibase.pro.packaged.A;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import xyz.fomichev.sandbox.BaseTest;
+import xyz.fomichev.sandbox.TestDataCreator;
+import xyz.fomichev.sandbox.model.Book;
+import xyz.fomichev.sandbox.model.BookInstance;
 import xyz.fomichev.sandbox.model.ReadingSession;
+import xyz.fomichev.sandbox.model.Student;
 import xyz.fomichev.sandbox.repository.ReadingSessionRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,14 +26,31 @@ public class ReadingSessionServiceTest extends BaseTest {
 
     @Autowired
     private ReadingSessionRepository readingSessionRepository;
+    @Autowired
+    private TestDataCreator testDataCreator;
+    @Autowired
+    private EntityManager entityManager;
+
+    private BookInstance bookInstance;
+    private Student student;
+
+    @BeforeEach
+    void setUp() {
+        var book = testDataCreator.book();
+        this.bookInstance = testDataCreator.bookInstance(book);
+        this.student = testDataCreator.student();
+    }
 
     @Test
     void testInstanceAccuracy() {
         var session = new ReadingSession();
         session.setId(readingSessionId);
         session.setStartTime(readingSessionStartDate);
-        readingSessionRepository.save(session);
-        var persistedSession = readingSessionRepository.findById(readingSessionId).orElseThrow();
+        session.setBookInstance(this.bookInstance);
+        session.setStudent(this.student);
+        var persistedSession = readingSessionRepository.save(session);
+        entityManager.flush();
+        entityManager.refresh(persistedSession);
         assertEquals(session.getId(), persistedSession.getId());
         assertEquals(session.getStartTime().truncatedTo(ChronoUnit.MICROS), persistedSession.getStartTime());
     }
