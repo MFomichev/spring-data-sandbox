5de80e4ae69e31c4a1062f02d40b32ddddd51ece
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-08-01 14:53:34 +0300

    reorganize ReadingSessionService

--- src/test/java/xyz/fomichev/sandbox/repository/ReadingSessionRepositoryTest.java
+++ src/test/java/xyz/fomichev/sandbox/repository/ReadingSessionRepositoryTest.java
@@ -1,25 +1,21 @@
-package xyz.fomichev.sandbox.service;
+package xyz.fomichev.sandbox.repository;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import liquibase.pro.packaged.A;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import xyz.fomichev.sandbox.BaseTest;
 import xyz.fomichev.sandbox.TestDataCreator;
-import xyz.fomichev.sandbox.model.Book;
 import xyz.fomichev.sandbox.model.BookInstance;
 import xyz.fomichev.sandbox.model.ReadingSession;
 import xyz.fomichev.sandbox.model.Student;
-import xyz.fomichev.sandbox.repository.ReadingSessionRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ReadingSessionServiceTest extends BaseTest {
+public class ReadingSessionRepositoryTest extends BaseTest {
 
     private UUID readingSessionId = UUID.fromString("ef76da29-9e8a-4c49-a7e5-93c926521198");
     private Instant readingSessionStartDate = Instant.parse("2007-12-03T23:59:59.999999999Z");
@@ -48,10 +44,10 @@ public class ReadingSessionServiceTest extends BaseTest {
         session.setStartTime(readingSessionStartDate);
         session.setBookInstance(this.bookInstance);
         session.setStudent(this.student);
-        var persistedSession = readingSessionRepository.save(session);
-        entityManager.flush();
+        var persistedSession = readingSessionRepository.saveAndFlush(session);
         entityManager.refresh(persistedSession);
         assertEquals(session.getId(), persistedSession.getId());
         assertEquals(session.getStartTime().truncatedTo(ChronoUnit.MICROS), persistedSession.getStartTime());
     }
+
 }
