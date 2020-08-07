784a4d423d2bcef0e5df41658dec5cb633bcfbe2
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-08-07 22:59:55 +0300

    add test for concurrent reading book

--- src/main/java/xyz/fomichev/sandbox/service/StudentService.java
+++ src/main/java/xyz/fomichev/sandbox/service/StudentService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.util.UUID;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.fomichev.sandbox.model.ReadingSession;
@@ -19,14 +20,14 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
     private final BookRepository bookRepository;
-    private final BookInstanceRepository bookInstanceRepository;
+    private final ObjectProvider<BookInstanceRepository> bookInstanceRepository;
     private final ReadingSessionRepository readingSessionRepository;
 
     public ReadingSession startRead(@NonNull UUID studentId, @NonNull UUID bookId) {
         var student = studentRepository.findById(studentId).orElseThrow();
         var book = bookRepository.findById(bookId).orElseThrow();
         //TODO: lock on book must be here
-        var bookInstance = bookInstanceRepository.findFreeInstanceByBookId(book.getId())
+        var bookInstance = bookInstanceRepository.getIfAvailable().findFreeInstanceByBookId(book.getId())
                 .orElseThrow(() -> new IllegalArgumentException("No free books"));
 
         ReadingSession readingSession = new ReadingSession();
--- src/test/java/xyz/fomichev/sandbox/service/StudentServiceNonTranTest.java
+++ src/test/java/xyz/fomichev/sandbox/service/StudentServiceNonTranTest.java
@@ -1,21 +1,92 @@
 package xyz.fomichev.sandbox.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import xyz.fomichev.sandbox.TransactionalBaseTest;
+import org.springframework.transaction.support.TransactionTemplate;
+import xyz.fomichev.sandbox.NonTransactionalBaseTest;
+import xyz.fomichev.sandbox.TestDataCreator;
+import xyz.fomichev.sandbox.model.Book;
+import xyz.fomichev.sandbox.model.BookInstance;
+import xyz.fomichev.sandbox.model.Student;
 import xyz.fomichev.sandbox.repository.BookInstanceRepository;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 
-class StudentServiceNonTranTest extends TransactionalBaseTest {
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static xyz.fomichev.sandbox.TestDataFactory.aStudent;
+
+class StudentServiceNonTranTest extends NonTransactionalBaseTest {
+
+    public static final UUID SECOND_STUDENT_ID = UUID.fromString("ef76da29-9e8a-4c49-a7e5-93c926593422");
 
     @Autowired
-    ApplicationContext context;
+    ObjectProvider<BookInstanceRepository> bookInstanceRepository;
+
+    @Autowired
+    StudentService studentService;
+    @Autowired
+    TestDataCreator testDataCreator;
+    @Autowired
+    TransactionTemplate transactionTemplate;
+
+    Student studentOne;
+    Student studentTwo;
+    Book book;
+    BookInstance bookInstance;
+
+    @BeforeEach
+    void setUp() {
+        this.studentOne = testDataCreator.student();
+        this.studentTwo = testDataCreator.student(aStudent().id(SECOND_STUDENT_ID).build());
+        this.book = testDataCreator.book();
+        this.bookInstance = testDataCreator.bookInstance(this.book);
+    }
 
     @Test
-    void testMock() {
-        assertTrue(Mockito.mockingDetails(context.getBean(BookInstanceRepository.class)).isMock());
+    void testStartReadForConcurrentInstanceBook() throws Exception {
+        Object finishFirstTransaction = new Object();
+        CountDownLatch readLatch = new CountDownLatch(2);
+        var bookInstanceRepositoryMock = this.bookInstanceRepository.getIfAvailable();
+        when(bookInstanceRepositoryMock.findFreeInstanceByBookId(any()))
+                .thenAnswer((Answer<Optional<BookInstance>>) invocation -> {
+                    Optional<BookInstance> result = (Optional<BookInstance>) invocation.callRealMethod();
+                    readLatch.countDown();
+                    readLatch.await();
+                    synchronized (finishFirstTransaction) {
+                        return result;
+                    }
+                });
+        var transaction1 = CompletableFuture.runAsync(() -> {
+            synchronized (finishFirstTransaction) {
+                transactionTemplate.executeWithoutResult(status -> {
+                    studentService.startRead(studentOne.getId(), book.getId());
+                });
+            }
+        });
+        var transaction2 = CompletableFuture.runAsync(() -> {
+            transactionTemplate.executeWithoutResult(status -> {
+                studentService.startRead(studentTwo.getId(), book.getId());
+            });
+        });
+        transaction1.get();
+        try {
+            transaction2.get();
+            fail();
+        } catch (ExecutionException e) {
+            if (e.getCause().getClass() != IllegalArgumentException.class) {
+                fail(e);
+            }
+        } catch (Throwable e) {
+            fail(e);
+        }
     }
 }
