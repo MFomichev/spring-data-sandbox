c1de0652f4e6ab93e3c5d3d5f5333a3e1b21bcbf
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-08-10 22:18:58 +0300

    configure tearDown

--- src/test/java/xyz/fomichev/sandbox/NonTransactionalBaseTest.java
+++ src/test/java/xyz/fomichev/sandbox/NonTransactionalBaseTest.java
@@ -4,14 +4,29 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.fomichev.sandbox.model.BookInstance;
+import xyz.fomichev.sandbox.repository.*;
 
 public class NonTransactionalBaseTest extends BaseTest {
 
     @Autowired
-    List<CrudRepository<?, ?>> repositories;
+    ReadingSessionRepository readingSessionRepository;
+    @Autowired
+    BookInstanceRepository bookInstanceRepository;
+    @Autowired
+    BookRepository bookRepository;
+    @Autowired
+    AuthorRepository authorRepository;
+    @Autowired
+    StudentRepository studentRepository;
 
     @AfterEach
     void tearDown() {
-        repositories.forEach(CrudRepository::deleteAll);
+        readingSessionRepository.deleteAll();
+        bookInstanceRepository.deleteAll();
+        bookRepository.deleteAll();
+        authorRepository.deleteAll();
+        studentRepository.deleteAll();
     }
 }
