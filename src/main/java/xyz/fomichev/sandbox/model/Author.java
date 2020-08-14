8bcb45021f404ad3f7e1ed76addd25351d205385
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-08-14 11:15:22 +0300

    add Author -> Regalia link

--- src/main/java/xyz/fomichev/sandbox/model/Author.java
+++ src/main/java/xyz/fomichev/sandbox/model/Author.java
@@ -1,10 +1,9 @@
 package xyz.fomichev.sandbox.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Version;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.NonNull;
@@ -20,6 +19,13 @@ public class Author {
 
     @Column(name = "last_name")
     private String lastName;
+
+    @OneToMany(
+            mappedBy = "author",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Regalia> regalias = new ArrayList<>();
 
     @Version
     private Integer version;
